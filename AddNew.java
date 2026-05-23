452319dacfdf315c4ccc35e7eb55fdc88e4060c4
Sindhuraavi2023 <sindhuraavi14@gmail.com>
2026-05-23 11:06:21 +0530

    Create AddNew.java

---
+++ AddNew.java
@@ -0,0 +1,10 @@
+public class AddNew {
+    public static void main(String[] args) {
+        int a = 5;  
+        int b = 7;  
+        int c = a + b;
+        int d = 35;
+
+        System.out.println(c);  t
+    }
+}
